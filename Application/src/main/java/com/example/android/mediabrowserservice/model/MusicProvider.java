0aaec0a4d5854d55d973a7ccc48fa975dc3810e4
mjuarez <mjuarez@yammer-inc.com>
2016-06-27 22:57:26 -0700

    Using hardcoded paths and an index json file to load all the songs from the cloud

--- Application/src/main/java/com/example/android/mediabrowserservice/model/MusicProvider.java
+++ Application/src/main/java/com/example/android/mediabrowserservice/model/MusicProvider.java
@@ -57,8 +57,9 @@ public class MusicProvider {
 
     private static final String TAG = LogHelper.makeLogTag(MusicProvider.class);
 
-    private static final String CATALOG_URL =
-        "http://storage.googleapis.com/automotive-media/music.json";
+//    private static final String CATALOG_URL =
+//        "http://storage.googleapis.com/automotive-media/music.json";
+    private static final String CATALOG_URL = "https://firebasestorage.googleapis.com/v0/b/project-8042746893150109988.appspot.com/o/music.json?alt=media&token=d73992ee-4b89-4ff2-b93e-5133961f6d83";
 
     public static final String CUSTOM_METADATA_TRACK_SOURCE = "__SOURCE__";
 
@@ -213,61 +214,61 @@ public class MusicProvider {
         }
 
         // get file Url
-        final StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/Come Thou fount of Every Blessing.mp3");
-        storageRef.getDownloadUrl()
-                .addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        Log.d(TAG, "getDownloadUrl:onSuccess:");
-//                        pathToSong = uri;
-                        if (mCurrentState == State.NON_INITIALIZED) {
-                            mCurrentState = State.INITIALIZING;
-                            MediaMetadata item = new MediaMetadata.Builder()
-                                    .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, String.valueOf(storageRef.getName().hashCode()))
-                                    .putString(CUSTOM_METADATA_TRACK_SOURCE, uri.toString())
-                                    .putString(MediaMetadata.METADATA_KEY_ALBUM, "Vino Jams Album")
-                                    .putString(MediaMetadata.METADATA_KEY_ARTIST, "Vino Jams")
-                                    .putLong(MediaMetadata.METADATA_KEY_DURATION, 221000)
-                                    .putString(MediaMetadata.METADATA_KEY_GENRE, "Rock")
-//                                .putString(MediaMetadata.METADATA_KEY_ALBUM_ART_URI, iconUrl)
-                                    .putString(MediaMetadata.METADATA_KEY_TITLE, storageRef.getName())
-                                    .putLong(MediaMetadata.METADATA_KEY_TRACK_NUMBER, 1)
-                                    .putLong(MediaMetadata.METADATA_KEY_NUM_TRACKS, 1)
-                                    .build();
-                            String musicId = item.getString(MediaMetadata.METADATA_KEY_MEDIA_ID);
-                            mMusicListById.put(musicId, new MutableMediaMetadata(musicId, item));
-
-                            buildListsByGenre();
-
-                            mCurrentState = State.INITIALIZED;
-                        }
-                        if (callback != null) {
-                            callback.onMusicCatalogReady(true);
-                        }
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.w(TAG, "getDownloadUrl:onFailure:", e);
-                    }
-                });
+//        final StorageReference storageRef = firebaseStorage.getReferenceFromUrl("gs://project-8042746893150109988.appspot.com/Tracks/Thea & Miguel - Yellow (Coldplay Cover).mp3");
+//        storageRef.getDownloadUrl()
+//                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+//                    @Override
+//                    public void onSuccess(Uri uri) {
+//                        Log.d(TAG, "getDownloadUrl:onSuccess:");
+////                        pathToSong = uri;
+//                        if (mCurrentState == State.NON_INITIALIZED) {
+//                            mCurrentState = State.INITIALIZING;
+//                            MediaMetadata item = new MediaMetadata.Builder()
+//                                    .putString(MediaMetadata.METADATA_KEY_MEDIA_ID, String.valueOf(storageRef.getName().hashCode()))
+//                                    .putString(CUSTOM_METADATA_TRACK_SOURCE, uri.toString())
+//                                    .putString(MediaMetadata.METADATA_KEY_ALBUM, "Vino Jams Album")
+//                                    .putString(MediaMetadata.METADATA_KEY_ARTIST, "Vino Jams")
+//                                    .putLong(MediaMetadata.METADATA_KEY_DURATION, 221000)
+//                                    .putString(MediaMetadata.METADATA_KEY_GENRE, "Rock")
+////                                .putString(MediaMetadata.METADATA_KEY_ALBUM_ART_URI, iconUrl)
+//                                    .putString(MediaMetadata.METADATA_KEY_TITLE, storageRef.getName())
+//                                    .putLong(MediaMetadata.METADATA_KEY_TRACK_NUMBER, 1)
+//                                    .putLong(MediaMetadata.METADATA_KEY_NUM_TRACKS, 1)
+//                                    .build();
+//                            String musicId = item.getString(MediaMetadata.METADATA_KEY_MEDIA_ID);
+//                            mMusicListById.put(musicId, new MutableMediaMetadata(musicId, item));
+//
+//                            buildListsByGenre();
+//
+//                            mCurrentState = State.INITIALIZED;
+//                        }
+//                        if (callback != null) {
+//                            callback.onMusicCatalogReady(true);
+//                        }
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.w(TAG, "getDownloadUrl:onFailure:", e);
+//                    }
+//                });
 
         // Asynchronously load the music catalog in a separate thread
-//        new AsyncTask<Void, Void, State>() {
-//            @Override
-//            protected State doInBackground(Void... params) {
-//                retrieveMedia();
-//                return mCurrentState;
-//            }
-//
-//            @Override
-//            protected void onPostExecute(State current) {
-//                if (callback != null) {
-//                    callback.onMusicCatalogReady(current == State.INITIALIZED);
-//                }
-//            }
-//        }.execute();
+        new AsyncTask<Void, Void, State>() {
+            @Override
+            protected State doInBackground(Void... params) {
+                retrieveMedia();
+                return mCurrentState;
+            }
+
+            @Override
+            protected void onPostExecute(State current) {
+                if (callback != null) {
+                    callback.onMusicCatalogReady(current == State.INITIALIZED);
+                }
+            }
+        }.execute();
     }
 
     private synchronized void buildListsByGenre() {
@@ -290,8 +291,6 @@ public class MusicProvider {
             if (mCurrentState == State.NON_INITIALIZED) {
                 mCurrentState = State.INITIALIZING;
 
-                int slashPos = CATALOG_URL.lastIndexOf('/');
-                String path = CATALOG_URL.substring(0, slashPos + 1);
                 JSONObject jsonObj = fetchJSONFromUrl(CATALOG_URL);
                 if (jsonObj == null) {
                     return;
@@ -299,7 +298,7 @@ public class MusicProvider {
                 JSONArray tracks = jsonObj.getJSONArray(JSON_MUSIC);
                 if (tracks != null) {
                     for (int j = 0; j < tracks.length(); j++) {
-                        MediaMetadata item = buildFromJSON(tracks.getJSONObject(j), path);
+                        MediaMetadata item = buildFromJSON(tracks.getJSONObject(j));
                         String musicId = item.getString(MediaMetadata.METADATA_KEY_MEDIA_ID);
                         mMusicListById.put(musicId, new MutableMediaMetadata(musicId, item));
                     }
@@ -318,26 +317,18 @@ public class MusicProvider {
         }
     }
 
-    private MediaMetadata buildFromJSON(JSONObject json, String basePath) throws JSONException {
+    private MediaMetadata buildFromJSON(JSONObject json) throws JSONException {
         String title = json.getString(JSON_TITLE);
         String album = json.getString(JSON_ALBUM);
         String artist = json.getString(JSON_ARTIST);
         String genre = json.getString(JSON_GENRE);
         String source = json.getString(JSON_SOURCE);
-        String iconUrl = json.getString(JSON_IMAGE);
         int trackNumber = json.getInt(JSON_TRACK_NUMBER);
         int totalTrackCount = json.getInt(JSON_TOTAL_TRACK_COUNT);
         int duration = json.getInt(JSON_DURATION) * 1000; // ms
 
         LogHelper.d(TAG, "Found music track: ", json);
 
-        // Media is stored relative to JSON file
-        if (!source.startsWith("http")) {
-            source = basePath + source;
-        }
-        if (!iconUrl.startsWith("http")) {
-            iconUrl = basePath + iconUrl;
-        }
         // Since we don't have a unique ID in the server, we fake one using the hashcode of
         // the music source. In a real world app, this could come from the server.
         String id = String.valueOf(source.hashCode());
@@ -353,7 +344,6 @@ public class MusicProvider {
                 .putString(MediaMetadata.METADATA_KEY_ARTIST, artist)
                 .putLong(MediaMetadata.METADATA_KEY_DURATION, duration)
                 .putString(MediaMetadata.METADATA_KEY_GENRE, genre)
-                .putString(MediaMetadata.METADATA_KEY_ALBUM_ART_URI, iconUrl)
                 .putString(MediaMetadata.METADATA_KEY_TITLE, title)
                 .putLong(MediaMetadata.METADATA_KEY_TRACK_NUMBER, trackNumber)
                 .putLong(MediaMetadata.METADATA_KEY_NUM_TRACKS, totalTrackCount)
